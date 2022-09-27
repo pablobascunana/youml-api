705c880af435d2ea549b35dbd2f20be718f3d63c
pbascunana <pablobascunanasaiz@gmail.com>
2022-09-27 22:54:03 +0200

    ML-28 Add role parameter in create user

--- src/users/viewsets/user/repository.py
+++ src/users/viewsets/user/repository.py
@@ -2,8 +2,8 @@ from django.contrib.auth.base_user import BaseUserManager
 
 
 class UserRepository(BaseUserManager):
-    def create_user(self, username, name, lastname, email, password):
-        user = self.model(name=name, lastname=lastname, email=self.normalize_email(email), username=username)
+    def create_user(self, username, name, lastname, email, password, role):
+        user = self.model(name=name, lastname=lastname, email=self.normalize_email(email), username=username, role=role)
         user.set_password(password)
         user.save(using=self._db)
         return user
