2045b8b540ca750180f86f7a4fb4ad2a054254b2
pbascunana <pablobascunanasaiz@gmail.com>
2022-09-30 07:20:55 +0200

    ML-27 Configure api urls

--- src/main/urls.py
+++ src/main/urls.py
@@ -1,5 +1,9 @@
 from django.urls import include, path
 
+api_version = '/v1/'
+
+
 urlpatterns = [
-    path('users/v1/', include("users.urls"))
+    path(f"users{api_version}", include("users.urls")),
+    path(f"api{api_version}", include("api.urls"))
 ]
