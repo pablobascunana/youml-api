1aa5a4f8156d9d841160a786c6521d98cbd0bf5d
pbascunana <pablobascunanasaiz@gmail.com>
2022-10-01 09:46:50 +0200

    ML-27 Postal code as camelCase

--- src/api/viewsets/company/serializer.py
+++ src/api/viewsets/company/serializer.py
@@ -4,6 +4,8 @@ from api.viewsets.company.model import Company
 
 
 class CompanySerializer(serializers.ModelSerializer):
+    postalCode = serializers.IntegerField(source="postal_code")
+
     class Meta:
         model = Company
-        fields = ('uuid', 'name', 'cif', 'email', 'address', 'city', 'country', 'postal_code', 'sector')
+        fields = ('uuid', 'name', 'cif', 'email', 'address', 'city', 'country', 'postalCode', 'sector')
