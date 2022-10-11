62fe415856bf13886887ba9b5190398e94afb50d
pbascunana <pablobascunanasaiz@gmail.com>
2022-10-11 17:05:01 +0200

    ML-19 Add auth view, set URL and implement login

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,5 +1,6 @@
 from rest_framework.routers import SimpleRouter
 
+from .viewsets.auth.view import AuthViewSet
 from .viewsets.user.view import UserViewSet
 
 router = SimpleRouter(trailing_slash=False)
@@ -7,5 +8,6 @@ router = SimpleRouter(trailing_slash=False)
 urlpatterns = []
 
 router.register("user", UserViewSet)
+router.register("auth", AuthViewSet)
 
 urlpatterns += router.urls
---
+++ src/users/viewsets/auth/view.py
@@ -0,0 +1,36 @@
+from django.contrib.auth import authenticate
+from django.utils import timezone
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+
+from users.models import User
+from core.permissions import IsAllowed
+from users.viewsets.user.serializer import UserSerializer
+from users.viewsets.user.service import RegisterUserService
+
+
+ATTEMPTS = 5
+
+
+class AuthViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAllowed]
+
+    user_service = RegisterUserService()
+
+    def create(self, request, *args, **kwargs):
+        if user := authenticate(username=request.data['username'], password=request.data['password']):
+            if user.active:
+                user.last_login = timezone.now()
+                self.user_service.update_login_attempts(user, 0)
+                return Response(status=status.HTTP_200_OK)
+
+        user = self.get_queryset().filter(username=request.data['username'])
+        if user.exists() and user.login_attempts < ATTEMPTS:
+            attempts = user.login_attempts + 1
+            self.user_service.update_login_attempts(user, attempts)
+            if user.login_attempts == ATTEMPTS:
+                self.user_service.activate_or_deactivate_user(user, False)
+
+        return Response(status=status.HTTP_403_FORBIDDEN)
