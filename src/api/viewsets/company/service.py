172d63a8bf3c84a08fd7e6064640292b39314383
pbascunana <pablobascunanasaiz@gmail.com>
2022-10-14 08:45:48 +0200

    ML-50 Add project service

--- src/api/viewsets/company/service.py
+++ src/api/viewsets/company/service.py
@@ -9,7 +9,7 @@ from api.viewsets.company.serializer import CompanySerializer
 class CompanyService:
 
     @staticmethod
-    def create_company(company: QueryDict) -> Dict:
+    def create_company(company: QueryDict) -> Company:
         company_serializer = CompanySerializer(data=company)
         company_serializer.is_valid(raise_exception=True)
         return Company.objects.create(**company_serializer.validated_data)
