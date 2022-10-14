bb34e8d3a68294e3a4d9a51fe315aa688b8b7f5d
pbascunana <pablobascunanasaiz@gmail.com>
2022-10-14 08:46:16 +0200

    ML-50 Implement project endpoint

--- src/api/viewsets/project/view.py
+++ src/api/viewsets/project/view.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 
 from api.viewsets.project.model import Project
 from api.viewsets.project.serializer import ProjectSerializer
+from api.viewsets.project.service import ProjectService
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
@@ -11,5 +12,6 @@ class ProjectViewSet(viewsets.ModelViewSet):
 
     @staticmethod
     def create(request, *args, **kwargs):
-        name = request.data['name']
-        return Response({}, status=status.HTTP_201_CREATED)
+        project = {'name': request.data['name'], 'user': request.user.uuid}
+        created_project = ProjectService().create_project(project)
+        return Response(created_project, status=status.HTTP_201_CREATED)
