8e4889dcebf291d732bcc49fb67435c8ddb0d15a
pbascunana <pablobascunanasaiz@gmail.com>
2022-10-14 07:43:51 +0200

    ML-50 Add project url configuration

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,11 +1,13 @@
 from rest_framework.routers import SimpleRouter
 
 from .viewsets.company.view import CompanyViewSet
+from .viewsets.project.view import ProjectViewSet
 
 router = SimpleRouter(trailing_slash=False)
 
 urlpatterns = []
 
 router.register("company", CompanyViewSet)
+router.register("project", ProjectViewSet)
 
 urlpatterns += router.urls
