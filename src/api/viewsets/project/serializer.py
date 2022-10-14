3407a74fcdd2d3340c8aae816ef0fb81592ee162
pbascunana <pablobascunanasaiz@gmail.com>
2022-10-14 08:47:25 +0200

    ML-50 Add to representation method in serializer

--- src/api/viewsets/project/serializer.py
+++ src/api/viewsets/project/serializer.py
@@ -5,6 +5,14 @@ from api.viewsets.project.model import Project
 
 class ProjectSerializer(serializers.ModelSerializer):
 
+    def to_representation(self, value):
+        return {
+            "uuid": value.uuid,
+            "name": value.name,
+            "creationDate": value.creation_date.strftime("%d-%m-%Y %H:%M:%S"),
+            "user": value.user.uuid
+        }
+
     class Meta:
         model = Project
         fields = '__all__'
