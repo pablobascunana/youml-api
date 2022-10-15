e55f22c8bc83d81c3b9aa273ac435021089b2bbe
pbascunana <pablobascunanasaiz@gmail.com>
2022-10-15 10:21:18 +0200

    ML-51 Add project unit tests

--- src/tests/api/test_project.py
+++ src/tests/api/test_project.py
@@ -10,28 +10,34 @@ class TestUserEndpoints:
     endpoint = '/api/v1/project'
     unique_fields = 'The fields user, name must make a unique set.'
 
-    def test_list(self, client_as_admin: APIClient):
+    PROJECT_NAME = 'My project'
+
+    def test_list_as_admin_user(self, client_as_admin: APIClient):
         response = client_as_admin[0].get(self.endpoint)
-        assert response.status_code == 403
+        assert response.status_code == 200
+
+    def test_list_as_normal_user(self, client_as_user: APIClient):
+        response = client_as_user[0].get(self.endpoint)
+        assert response.status_code == 200
 
     def test_list_projects_without_permission(self, client: APIClient):
         response = client.get(self.endpoint)
         assert response.status_code == 403
 
     def test_create_project(self, client_as_user: APIClient):
-        body = {"name": 'My project', "user": str(client_as_user[1].uuid)}
+        body = {"name": self.PROJECT_NAME, "user": str(client_as_user[1].uuid)}
         response = client_as_user[0].post(f"{self.endpoint}", body, format='json')
         assert response.status_code == 201
 
     def test_create_project_with_existing_name(self, client_as_user: APIClient):
-        baker.make(Project, name='My project', user=client_as_user[1])
-        body = {"name": 'My project', "user": str(client_as_user[1].uuid)}
+        baker.make(Project, name=self.PROJECT_NAME, user=client_as_user[1])
+        body = {"name": self.PROJECT_NAME, "user": str(client_as_user[1].uuid)}
         response = client_as_user[0].post(f"{self.endpoint}", body, format='json')
         assert response.status_code == 400
         assert response.json()['non_field_errors'][0] == self.unique_fields
 
     def test_create_project_without_permissions(self, client: APIClient):
-        body = {"name": 'My project', "user": 'abcd-efg'}
+        body = {"name": self.PROJECT_NAME, "user": 'abcd-efg'}
         response = client.post(f"{self.endpoint}", body, format='json')
         assert response.status_code == 403
 
