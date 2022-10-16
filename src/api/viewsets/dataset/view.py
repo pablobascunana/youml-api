3b2a1e39cd6e194208a8f8494a8d028f5994219e
pbascunana <pablobascunanasaiz@gmail.com>
2022-10-16 12:04:48 +0200

    ML-62 Add dataset view and create endpoint

---
+++ src/api/viewsets/dataset/view.py
@@ -0,0 +1,17 @@
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+
+from api.viewsets.dataset.model import Dataset
+from api.viewsets.dataset.serializer import DatasetSerializer
+from api.viewsets.dataset.service import DatasetService
+
+
+class DatasetViewSet(viewsets.ModelViewSet):
+    queryset = Dataset.objects.all()
+    serializer_class = DatasetSerializer
+
+    @staticmethod
+    def create(request, *args, **kwargs):
+        dataset = {'name': request.data['name'], 'user': request.user.uuid, 'project': request.data['project']}
+        created_dataset = DatasetService().create_dataset(dataset)
+        return Response(created_dataset, status=status.HTTP_201_CREATED)
