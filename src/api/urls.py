1a31f2dbad3c3330efd6e66be8712a19cb7686cc
pbascunana <pablobascunanasaiz@gmail.com>
2022-10-16 12:04:40 +0200

    ML-62 Register dataset URL

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,6 +1,8 @@
+from django.urls import path
 from rest_framework.routers import SimpleRouter
 
 from .viewsets.company.view import CompanyViewSet
+from .viewsets.dataset.view import DatasetViewSet
 from .viewsets.project.view import ProjectViewSet
 
 router = SimpleRouter(trailing_slash=False)
@@ -9,5 +11,6 @@ urlpatterns = []
 
 router.register("company", CompanyViewSet)
 router.register("project", ProjectViewSet)
+router.register("dataset", DatasetViewSet)
 
 urlpatterns += router.urls
