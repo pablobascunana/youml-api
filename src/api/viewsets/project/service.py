f3381ae894fe1d7a449d30c94301c13ae8edb12b
pbascunana <pablobascunanasaiz@gmail.com>
2022-10-17 07:27:56 +0200

    ML-68 Add delete endpoint and service

--- src/api/viewsets/project/service.py
+++ src/api/viewsets/project/service.py
@@ -1,13 +1,18 @@
 from typing import Dict
 
+from api.viewsets import Project
 from api.viewsets.project.serializer import ProjectSerializer
 
 
 class ProjectService:
 
     @staticmethod
-    def create_project(project: Dict) -> Dict:
+    def create(project: Dict) -> Dict:
         project_serializer = ProjectSerializer(data=project)
         project_serializer.is_valid(raise_exception=True)
         project_serializer.save()
         return project_serializer.data
+
+    @staticmethod
+    def delete(project_uuid: str):
+        return Project.objects.filter(pk=project_uuid).delete()[0]
--- src/api/viewsets/project/view.py
+++ src/api/viewsets/project/view.py
@@ -11,6 +11,8 @@ class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
 
+    project_service = ProjectService()
+
     def list(self, request, *args, **kwargs):
         if request.user.role == 'ADMIN' and request.user.company:
             users = RegisterUserService().get_users_by_company_id(request.user.company_id)
@@ -21,8 +23,12 @@ class ProjectViewSet(viewsets.ModelViewSet):
         json_projects = ProjectSerializer(projects, many=True).data
         return Response(json_projects, status=status.HTTP_200_OK)
 
-    @staticmethod
-    def create(request, *args, **kwargs):
+    def create(self, request, *args, **kwargs):
         project = {'name': request.data['name'], 'user': request.user.uuid}
-        created_project = ProjectService().create_project(project)
+        created_project = self.project_service.create(project)
         return Response(created_project, status=status.HTTP_201_CREATED)
+
+    def destroy(self, request, pk=None, *args, **kwargs):
+        destroyed = self.project_service.delete(pk)
+        return Response(status=status.HTTP_204_NO_CONTENT) if destroyed > 0 \
+            else Response("Resource not found", status=status.HTTP_404_NOT_FOUND)
