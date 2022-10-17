6c1e3bfa3e67bc66aadb1352062e7581343f228f
pbascunana <pablobascunanasaiz@gmail.com>
2022-10-17 08:36:53 +0200

    ML-69 Update conftests file

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,5 +1,5 @@
-from tests.fixtures.users import user_admin, user, inactive_user  # noqa: F401
-from tests.fixtures.http_clients import client_as_admin, client_as_user, client_as_inactive_user, client  # noqa: F401
-from tests.fixtures.companies import company, company_user  # noqa: F401
-from tests.fixtures.project import project  # noqa: F401
-from tests.fixtures.dataset import dataset  # noqa: F401
+from tests.fixtures.users import user_admin, user, inactive_user  # noqa: F401 pylint: disable=unused-import
+from tests.fixtures.http_clients import client_as_admin, client_as_user, client_as_inactive_user, client  # noqa: F401 pylint: disable=unused-import
+from tests.fixtures.companies import company, company_user  # noqa: F401 pylint: disable=unused-import
+from tests.fixtures.project import project  # noqa: F401 pylint: disable=unused-import
+from tests.fixtures.dataset import dataset  # noqa: F401 pylint: disable=unused-import
