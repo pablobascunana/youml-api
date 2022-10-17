4d9c8f0de4ac5f3d7be8877e6c1e31d1b44d2f15
pbascunana <pablobascunanasaiz@gmail.com>
2022-10-17 08:18:04 +0200

    ML-69 Add dataset fixture

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -2,3 +2,4 @@ from tests.fixtures.users import user_admin, user, inactive_user # noqa
 from tests.fixtures.http_clients import client_as_admin, client_as_user, client_as_inactive_user, client # noqa
 from tests.fixtures.companies import company, company_user # noqa
 from tests.fixtures.project import project # noqa
+from tests.fixtures.dataset import dataset # noqa
