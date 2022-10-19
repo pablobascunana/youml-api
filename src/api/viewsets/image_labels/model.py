6d42519158b5087b3a25dd636cec9b3c6f7099ba
pbascunana <pablobascunanasaiz@gmail.com>
2022-10-19 22:56:38 +0200

    ML-78 Add imageLabels model

---
+++ src/api/viewsets/image_labels/model.py
@@ -0,0 +1,18 @@
+import uuid
+
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from api.mixins.validate_model import ValidateModelMixin
+from api.viewsets import Label, Image
+
+
+class ImageLabels(ValidateModelMixin, models.Model):
+    uuid = models.UUIDField(_('uuid'), max_length=64, unique=True, primary_key=True, default=uuid.uuid4)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE, null=False, blank=False, db_column='image')
+    label = models.ForeignKey(Label, on_delete=models.CASCADE, null=False, blank=False, db_column='label')
+
+    REQUIRED_FIELDS = ['image', 'label']
+
+    class Meta:
+        db_table = 'image_labels'
