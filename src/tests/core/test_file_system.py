258d3b5cc61ef88e967a426e70b0c444ad4b5d4e
pbascunana <pablobascunanasaiz@gmail.com>
2022-10-21 00:16:39 +0200

    ML-92 Create file manager fixture and fix non self use

--- src/tests/core/test_file_system.py
+++ src/tests/core/test_file_system.py
@@ -10,52 +10,49 @@ from core.providers.file_system import FileManagerProvider
 @pytest.mark.django_db
 class TestLocalFileSystem:
 
-    def test_provider(self):
-        file_manager = FileManagerProvider(os.getenv('STORAGE_TYPE')).provider
+    filename = '.env.production'
+
+    @staticmethod
+    def test_provider(file_manager):
         assert isinstance(file_manager, LocalFileManager)
 
-    def test_exist_path(self):
-        file_manager = FileManagerProvider(os.getenv('STORAGE_TYPE')).provider
+    @staticmethod
+    def test_exist_path(file_manager):
         exists = file_manager.exist_path(settings.BASE_DIR)
         assert exists
 
-    def test_not_exist_path(self):
-        file_manager = FileManagerProvider(os.getenv('STORAGE_TYPE')).provider
+    @staticmethod
+    def test_not_exist_path(file_manager):
         exists = file_manager.exist_path(f'{settings.BASE_DIR}/folder')
         assert not exists
 
-    def test_create_directory(self, tmp_path):
-        file_manager = FileManagerProvider(os.getenv('STORAGE_TYPE')).provider
+    @staticmethod
+    def test_create_directory(file_manager, tmp_path):
         file_manager.create_directory(tmp_path)
         assert file_manager.exist_path(tmp_path)
 
-    def test_create_path(self, tmp_path):
-        file_manager = FileManagerProvider(os.getenv('STORAGE_TYPE')).provider
+    @staticmethod
+    def test_create_path(file_manager, tmp_path):
         full_path = file_manager.create_path(tmp_path, 'abc', 'def')
         assert file_manager.exist_path(full_path)
 
-    def test_format_filename(self):
-        file_manager = FileManagerProvider(os.getenv('STORAGE_TYPE')).provider
+    @staticmethod
+    def test_format_filename(file_manager):
         filename = file_manager.format_filename('my file.txt')
         assert filename == 'my_file.txt'
 
-    def test_save_file(self, tmp_path):
-        filename = '.env.production'
-        file_manager = FileManagerProvider(os.getenv('STORAGE_TYPE')).provider
-        file_bytes = file_manager.retrieve_file(f'{settings.BASE_DIR}/{filename}')
-        file_manager.save_file(f'{tmp_path}/{filename}', file_bytes)
-        assert file_manager.exist_path(f'{tmp_path}/{filename}')
+    def test_save_file(self, file_manager, tmp_path):
+        file_bytes = file_manager.retrieve_file(f'{settings.BASE_DIR}/{self.filename}')
+        file_manager.save_file(f'{tmp_path}/{self.filename}', file_bytes)
+        assert file_manager.exist_path(f'{tmp_path}/{self.filename}')
 
-    def test_retrieve_file(self):
-        filename = '.env.production'
-        file_manager = FileManagerProvider(os.getenv('STORAGE_TYPE')).provider
-        file_bytes = file_manager.retrieve_file(f'{settings.BASE_DIR}/{filename}')
+    def test_retrieve_file(self, file_manager):
+        file_bytes = file_manager.retrieve_file(f'{settings.BASE_DIR}/{self.filename}')
         assert isinstance(file_bytes, bytes)
 
-    def test_delete_file(self, tmp_path):
-        filename = '.env.production'
+    def test_delete_file(self, file_manager, tmp_path):
         file_manager = FileManagerProvider(os.getenv('STORAGE_TYPE')).provider
-        file_bytes = file_manager.retrieve_file(f'{settings.BASE_DIR}/{filename}')
-        file_manager.save_file(f'{tmp_path}/{filename}', file_bytes)
-        file_manager.delete_file(f'{tmp_path}/{filename}')
-        assert not file_manager.exist_path(f'{tmp_path}/{filename}')
+        file_bytes = file_manager.retrieve_file(f'{settings.BASE_DIR}/{self.filename}')
+        file_manager.save_file(f'{tmp_path}/{self.filename}', file_bytes)
+        file_manager.delete_file(f'{tmp_path}/{self.filename}')
+        assert not file_manager.exist_path(f'{tmp_path}/{self.filename}')
