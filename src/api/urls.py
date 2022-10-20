532d616491fce227bcebd968e33ed8287248ccfc
pbascunana <pablobascunanasaiz@gmail.com>
2022-10-20 06:35:10 +0200

    ML-79 Register label view

--- src/api/urls.py
+++ src/api/urls.py
@@ -2,6 +2,7 @@ from rest_framework.routers import SimpleRouter
 
 from .viewsets.company.view import CompanyViewSet
 from .viewsets.dataset.view import DatasetViewSet
+from .viewsets.label.view import LabelViewSet
 from .viewsets.project.view import ProjectViewSet
 
 router = SimpleRouter(trailing_slash=False)
@@ -11,5 +12,6 @@ urlpatterns = []
 router.register("company", CompanyViewSet)
 router.register("project", ProjectViewSet)
 router.register("dataset", DatasetViewSet)
+router.register("label", LabelViewSet)
 
 urlpatterns += router.urls
