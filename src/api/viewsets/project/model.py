dde25428090f7a1d0e173f5b0d073f5b083a31d2
pbascunana <pablobascunanasaiz@gmail.com>
2022-10-21 17:45:51 +0200

    ML-76 Add storage in field in project model

--- src/api/viewsets/project/model.py
+++ src/api/viewsets/project/model.py
@@ -11,6 +11,7 @@ class Project(ValidateModelMixin, models.Model):
     uuid = models.UUIDField(_('uuid'), max_length=64, unique=True, primary_key=True, default=uuid.uuid4)
     name = models.CharField(_('name'), max_length=30, null=False)
     created_at = models.DateTimeField(_('createdAt'), default=timezone.now, db_column='createdAt')
+    storage_in = models.CharField(_('storage_in'), max_length=80, null=True, blank=True)
     user = models.ForeignKey("users.User", on_delete=models.CASCADE, null=False, blank=False, db_column='user')
 
     REQUIRED_FIELDS = ['uuid', 'user', 'name']
