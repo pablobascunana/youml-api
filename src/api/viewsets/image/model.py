82dcad68a2e2ff05d29a3a242efb99d41707d963
pbascunana <pablobascunanasaiz@gmail.com>
2022-10-21 18:40:32 +0200

    ML-80 Remove path field in image model

--- src/api/viewsets/image/model.py
+++ src/api/viewsets/image/model.py
@@ -10,10 +10,9 @@ from api.viewsets.dataset.model import Dataset
 class Image(ValidateModelMixin, models.Model):
     uuid = models.UUIDField(_('uuid'), max_length=64, unique=True, primary_key=True, default=uuid.uuid4)
     name = models.CharField(_('name'), max_length=30, null=False)
-    path = models.CharField(_('path'), max_length=30, null=False)
     dataset = models.ForeignKey(Dataset, on_delete=models.CASCADE, null=False, blank=False, db_column='dataset')
 
-    REQUIRED_FIELDS = ['dataset', 'name', 'path']
+    REQUIRED_FIELDS = ['dataset', 'name']
 
     class Meta:
         db_table = 'image'
