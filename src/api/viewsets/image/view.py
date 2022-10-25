e270917e0872405621ed0fecfaab21a7038f6301
pbascunana <pablobascunanasaiz@gmail.com>
2022-10-25 07:51:24 +0200

    ML-114 Update service and image view to return created date

--- src/api/viewsets/image/view.py
+++ src/api/viewsets/image/view.py
@@ -19,8 +19,7 @@ class ImageViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     def list(request, *args, **kwargs):
         return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
-    @staticmethod
-    def create(request, *args, **kwargs):
+    def create(self, request, *args, **kwargs):
         project = ProjectService().get_by_id(request.data['project'])
         file_system = FileManagerProvider(os.getenv('STORAGE_TYPE')).provider
         filename = file_system.format_filename(request.data['filename'])
@@ -28,5 +27,5 @@ class ImageViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
         file_system.create_directory(filepath)
         file_system.save_file(f'{filepath}/{filename}', request.data['file'].read())
         image = {"name": filename, "dataset": request.data['dataset']}
-        ImageService().create(image)
-        return Response(status=status.HTTP_201_CREATED)
+        saved_image = ImageService().create(image)
+        return Response(self.get_serializer(saved_image).data, status=status.HTTP_201_CREATED)
