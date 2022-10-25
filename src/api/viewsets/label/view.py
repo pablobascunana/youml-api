b246d366069bc2acc7a6916655265f7ae874d477
pbascunana <pablobascunanasaiz@gmail.com>
2022-10-25 07:28:16 +0200

    ML-97 Add delete labels endpoint

--- src/api/viewsets/label/view.py
+++ src/api/viewsets/label/view.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 
+from api.viewsets import ImageLabels
 from api.viewsets.label.model import Label
 from api.viewsets.label.serializer import LabelSerializer
 
@@ -13,6 +14,10 @@ class LabelViewSet(viewsets.ModelViewSet):
     def update(request, pk=None, *args, **kwargs):
         return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
-    @staticmethod
-    def destroy(request, pk=None, *args, **kwargs):
-        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+    def destroy(self, request, pk=None, *args, **kwargs):
+        label = ImageLabels.objects.filter(label=pk)
+        if label.exists():
+            self.get_queryset().filter(pk=pk).delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
