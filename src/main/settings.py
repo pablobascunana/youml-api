9bd7ce7407040036901909aa243adc3d37e2898b
pbascunana <pablobascunanasaiz@gmail.com>
2022-10-28 22:58:47 +0200

    ML-100 Configure djangorestframework-camel-case

--- src/main/settings.py
+++ src/main/settings.py
@@ -51,7 +51,11 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication'
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'djangorestframework_camel_case.render.CamelCaseJSONRenderer',
+        'djangorestframework_camel_case.render.CamelCaseBrowsableAPIRenderer',
     ),
 }
 
