0bacec5b2b44447b16190ecb6b07aac7840149d9
pbascunana <pablobascunanasaiz@gmail.com>
2022-11-20 16:35:14 +0100

    ML-104 Add training unit tests

--- src/tests/fixtures/dataset.py
+++ src/tests/fixtures/dataset.py
@@ -1,9 +1,19 @@
 import pytest
 from model_bakery import baker
 
+from api.viewsets import Project, Label, Image, ImageLabels
 from api.viewsets.dataset.model import Dataset
 
 
 @pytest.fixture()
 def dataset() -> Dataset:
     return baker.make(Dataset)
+
+
+@pytest.fixture()
+def dataset_to_train(project: Project) -> Dataset:
+    dataset = baker.make(Dataset, project=project)
+    label = baker.make(Label)
+    image = baker.make(Image)
+    baker.make(ImageLabels, image=image, label=label)
+    return dataset
