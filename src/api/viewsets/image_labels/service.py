1c25ba5cf5976f0d50099b05eac5ae75c1b6264a
pbascunana <pablobascunanasaiz@gmail.com>
2022-11-20 15:56:00 +0100

    ML-104 Update mark to train in image labels service

--- src/api/viewsets/image_labels/service.py
+++ src/api/viewsets/image_labels/service.py
@@ -1,5 +1,6 @@
 from django.db.models import QuerySet
 
+from core.utils.query import do_bulk_update
 from api.viewsets import ImageLabels
 
 
@@ -8,3 +9,11 @@ class ImageLabelService:
     @staticmethod
     def filter_by_label_id(uuid: str) -> QuerySet:
         return ImageLabels.objects.filter(label=uuid)
+
+    @staticmethod
+    def get_mark_to_train_at_with_value_none():
+        return ImageLabels.objects.filter(mark_to_train_at__isnull=True)
+
+    def update_mark_to_train_at(self):
+        image_labels = self.get_mark_to_train_at_with_value_none()
+        do_bulk_update(image_labels, ImageLabels, ['mark_to_train_at'])
