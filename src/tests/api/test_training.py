06b50e723593fc04ae2abd378c75f0ad970fec68
pbascunana <pablobascunanasaiz@gmail.com>
2023-01-13 16:15:21 +0100

    ML-121 Fix training unit tests

--- src/tests/api/test_training.py
+++ src/tests/api/test_training.py
@@ -48,22 +48,12 @@ class TestProjectEndpoints:
         response = client.post(f"{self.mark_to_train_endpoint}", {"dataset": dataset_to_train.uuid}, format='json')
         assert response.status_code == 403
 
-    def test_success_train(self, client_as_user: APIClient, dataset_to_train: Dataset, mock_success_post):
+    def test_success_train(self, client_as_user: APIClient, dataset_to_train: Dataset):
         response = client_as_user[0].post(f"{self.endpoint}", {"dataset": dataset_to_train.uuid},
                                           format='json')
         assert response.status_code == 200
 
-    def test_forbidden_train(self, client_as_user: APIClient, dataset_to_train: Dataset, mock_forbidden_post):
-        response = client_as_user[0].post(f"{self.endpoint}", {"dataset": dataset_to_train.uuid},
-                                          format='json')
-        assert response.status_code == 403
-
-    def test_not_found_train(self, client_as_user: APIClient, dataset_to_train: Dataset, mock_not_found_post):
-        response = client_as_user[0].post(f"{self.endpoint}", {"dataset": dataset_to_train.uuid},
-                                          format='json')
-        assert response.status_code == 404
-
-    def test_train_without_permissions(self, client: APIClient, dataset_to_train: Dataset, mock_forbidden_post):
+    def test_train_without_permissions(self, client: APIClient, dataset_to_train: Dataset):
         response = client.post(f"{self.endpoint}", {"dataset": dataset_to_train.uuid}, format='json')
         assert response.status_code == 403
 
